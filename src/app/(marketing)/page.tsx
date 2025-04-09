56bde39f1c28c4c912f82fd4089210f9305094ca
mtdik0406 <48610054+mtdik0406@users.noreply.github.com>
2025-04-09 23:33:17 +0900

    feat: update home page

---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,43 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <section className="pt-6 md:pt-12 lg:py-32 pb-8 md:pb-12">
+        <div className="container mx-auto px-4 text-center flex flex-col items-center gap-4 max-w-[64rem]">
+          <Link
+            href="/"
+            className="bg-muted px-4 py-1.5 rounded-2xl font-medium text-sm "
+          >
+            Xをフォローする
+          </Link>
+          <h1 className="font-extrabold text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
+            Post Writer App
+          </h1>
+          <p className="text-muted-foreground sm:text-xl leading-normal max-w-[42rem]">
+            このアプリケーションはNext.js App
+            Routerで作られたものです。ユーザは自由に投稿をポストすることができます。
+          </p>
+          <div className="space-x-4">
+            <Link href="/login" className={cn(buttonVariants({ size: "lg" }))}>
+              はじめる
+            </Link>
+            <Link
+              href="/"
+              className={cn(buttonVariants({ size: "lg", variant: "outline" }))}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </Link>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default page;
