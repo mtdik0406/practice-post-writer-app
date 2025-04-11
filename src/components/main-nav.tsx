f572d8cbd4603ea04616c01874aba9f3eff93e89
mtdik0406 <48610054+mtdik0406@users.noreply.github.com>
2025-04-12 08:39:09 +0900

    feat: add marketing layout and navigation component

---
+++ src/components/main-nav.tsx
@@ -0,0 +1,29 @@
+import { NavItem } from "@/types";
+import Link from "next/link";
+import { ReactNode } from "react";
+
+interface MainNavProps {
+  items?: NavItem[];
+  children?: ReactNode;
+}
+
+export default function MainNav({ items }: MainNavProps) {
+  return (
+    <div className="flex items-center md:gap-10">
+      <Link href="/" className="hidden md:flex items-center gap-2">
+        <span className="font-bold hidden md:inline-block">Post Writer</span>
+      </Link>
+      <nav className="md:flex gap-6 hidden">
+        {items?.map((item, index) => (
+          <Link
+            key={index}
+            href={item.href}
+            className="text-lg sm:text-sm font-medium hover:text-foreground/80"
+          >
+            {item.title}
+          </Link>
+        ))}
+      </nav>
+    </div>
+  );
+}
---
+++ src/config/marketing.tsx
@@ -0,0 +1,14 @@
+import { MarketingSite } from "@/types";
+
+export const marketingConfig: MarketingSite = {
+  mainNav: [
+    {
+      title: "特徴",
+      href: "/feature",
+    },
+    {
+      title: "ブログ",
+      href: "/blog",
+    },
+  ],
+};
