bb716d1745baf2ba00459e0eabef3619eff06ba2
mtdik0406 <48610054+mtdik0406@users.noreply.github.com>
2025-04-13 06:00:59 +0900

    feat: add contentlayer configuration and blog posts

---
+++ contentlayer.config.ts
@@ -0,0 +1,31 @@
+import { defineDocumentType, makeSource } from 'contentlayer2/source-files';
+
+export const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: `blog/**/*.mdx`,
+  contentType: 'mdx',
+  fields: {
+    title: { type: 'string', required: true },
+    description: { type: 'string'},
+    date: { type: 'date', required: true },
+    published: { type: 'boolean', required: true },
+    image: { type: 'string', required: true},
+    authors: {
+      // Reference types are not embedded.
+      // Until this is fixed, we can use a simple list.
+      // type: "reference",
+      // of: Author,
+      type: "list",
+      of: { type: "string" },
+      required: true,
+    },
+  },
+  computedFields: {
+    slug: {
+      type: 'string',
+      resolve: (doc) => `/${doc._raw.flattenedPath}`,
+    },
+  },
+}));
+
+export default makeSource({ contentDirPath: './content', documentTypes: [Post] });
