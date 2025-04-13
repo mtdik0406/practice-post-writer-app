403b2b38a9ef7da9af23ead10d061275ad22d844
mtdik0406 <48610054+mtdik0406@users.noreply.github.com>
2025-04-13 17:20:04 +0900

    feat: add login page

---
+++ src/components/user-auth-form.tsx
@@ -0,0 +1,36 @@
+import { Icon } from "./icon";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+
+export default function UserAuthForm() {
+  return (
+    <div className="grid gap-6">
+      <form>
+        <div className="grid gap-2">
+          <div className="grid gap-1.5">
+            <Label htmlFor="email">メールアドレス</Label>
+            <Input type="email" id="email" placeholder="メールアドレスを入力" />
+          </div>
+          <Button>メールアドレスでログイン</Button>
+        </div>
+      </form>
+
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <div className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs">
+          <span className="bg-background px-2 text-muted-foreground">
+            または
+          </span>
+        </div>
+      </div>
+
+      <Button>
+        <Icon.github />
+        Githubでログイン
+      </Button>
+    </div>
+  );
+}
