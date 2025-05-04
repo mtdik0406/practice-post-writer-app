ed34bcdff6b5e62f09db10a77ff387e376a71833
mtdik0406 <48610054+mtdik0406@users.noreply.github.com>
2025-05-04 11:25:40 +0900

    feat: add editor component and layout for post editing, integrate EditorJS tools

---
+++ src/app/(editor)/editor/[postId]/page.tsx
@@ -0,0 +1,46 @@
+import Editor from "@/components/editor";
+import { db } from "@/lib/db";
+import { getCurrentUser } from "@/lib/session";
+import { Post, User } from "@prisma/client";
+import { redirect } from "next/navigation";
+
+interface EditorProps {
+  params: Promise<{ postId: string }>;
+}
+
+async function getPostForUser(postId: Post["id"], userId: User["id"]) {
+  const post = await db.post.findFirst({
+    where: {
+      id: postId,
+      authorId: userId,
+    },
+  });
+
+  return post;
+}
+
+export default async function EditorPage({ params }: EditorProps) {
+  const { postId } = await params;
+
+  const user = await getCurrentUser();
+  if (!user) {
+    redirect("/login");
+  }
+
+  const post = await getPostForUser(postId, user?.id);
+  if (!post) {
+    // TODO：エラー処理
+    return;
+  }
+
+  return (
+    <Editor
+      post={{
+        id: post?.id,
+        title: post?.title,
+        content: post?.content,
+        published: post?.published,
+      }}
+    />
+  );
+}
---
+++ src/components/editor.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "./ui/button";
+import TextareaAutosize from "react-textarea-autosize";
+import { useEffect, useRef } from "react";
+import type EditorJS from "@editorjs/editorjs";
+import Header from "@editorjs/header";
+import List from "@editorjs/list";
+import CodeTool from "@editorjs/code";
+import { Post } from "@prisma/client";
+
+interface EditorProps {
+  post: Pick<Post, "id" | "title" | "content" | "published">;
+}
+
+export default function Editor({ post }: EditorProps) {
+  const editorRef = useRef<EditorJS | null>(null);
+
+  useEffect(() => {
+    const initEditor = async () => {
+      const EditorJS = (await import("@editorjs/editorjs")).default;
+      const LinkTool = (await import("@editorjs/link")).default;
+
+      if (!editorRef.current) {
+        const editor = new EditorJS({
+          holder: "editor",
+          placeholder: "ここに記事を書く",
+          inlineToolbar: true,
+          tools: {
+            header: Header,
+            linkTool: LinkTool,
+            list: List,
+            code: CodeTool,
+          },
+          data: {
+            blocks: [],
+          },
+        });
+        editorRef.current = editor;
+      }
+    };
+
+    if (typeof window !== "undefined") {
+      initEditor();
+    }
+
+    return () => {
+      if (editorRef.current) {
+        editorRef.current?.destroy();
+        editorRef.current = null;
+      }
+    };
+  }, []);
+
+  return (
+    <form>
+      <div className="flex flex-col gap-10">
+        <div className="flex w-full items-center justify-between">
+          <div className="flex items-center space-x-10">
+            <Button variant="ghost" className="border">
+              <Link href={"/dashboard"}>戻る</Link>
+            </Button>
+            <p className="text-sm text-muted-foreground">公開</p>
+          </div>
+          <Button type="submit">
+            <span>保存</span>
+          </Button>
+        </div>
+        <div className="w-[800px] mx-auto">
+          <TextareaAutosize
+            id="title"
+            autoFocus
+            placeholder="Post Title"
+            defaultValue={post.title}
+            className="w-full resize-none overflow-hidden bg-transparent text-5xl focus:outline-none font-bold"
+          />
+        </div>
+        <div id="editor" className="min-h-[500px]"></div>
+        <p className="text-sm text-gray-500">
+          Use
+          <kbd className="rounded-b-md border bg-muted px-1 text-xs uppercase">
+            Tab
+          </kbd>
+          to open the command menu
+        </p>
+      </div>
+    </form>
+  );
+}
